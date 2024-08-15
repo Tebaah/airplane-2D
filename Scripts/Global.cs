02a800405c30d3442ce19cbfa932411001e3c399
Tebaah <e2nd@protonmail.com>
2024-08-15 11:55:17 -0400

    feat(enemy): semi boss)

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -4,5 +4,10 @@ using System;
 public partial class Global : Node
 {
     // variables
+
+    // vida del jugador
     public int life = 1;
+
+    // puntaje del jugador
+    public int score = 0;
 }
