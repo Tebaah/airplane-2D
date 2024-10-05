533f59a0689b2a4d0e2378a4e25fb948643e61f2
Tebaah <e2nd@protonmail.com>
2024-10-05 15:16:15 -0300

    feat(IntermediateEnemy):an intermediate enemy with the ability to move from left to right is added

---
+++ Scripts/Enemy/IntermediateEnemy.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class IntermediateEnemy : EnemyController
+{
+    #region variables
+
+    #endregion
+
+    #region metodos godot
+    public override void _Ready()
+    {
+        base._Ready();
+        SetLevel();
+        SetAttributes();   
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Movement(delta, 250);
+        Shoot();
+    }
+    #endregion
+
+    #region metodos
+
+    #endregion
+}
